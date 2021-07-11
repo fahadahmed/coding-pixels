bdee218525c368231f5d6e32577e6fbb0b4aa09a
Fahad Ahmed <fahad.ahmed@me.com>
2021-07-11 23:29:33 +1000

    CP-1 created a nextJS app for the frontend app

---
+++ packages/frontend/pages/index.js
@@ -0,0 +1,14 @@
+import { Animated, Basic, bounce, Combined } from '../shared/styles'
+
+const Home = () => (
+  <div>
+    <Basic>Cool Styles</Basic>
+    <Combined>
+      With <code>:hover</code>.
+    </Combined>
+    <Animated animation={bounce}>Let's bounce.</Animated>
+    <div>This is a test for hot reload.</div>
+  </div>
+)
+
+export default Home
