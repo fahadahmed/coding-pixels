35a8367344fbecccb18310210dc21a46f703e011
Fahad Ahmed <fahad.ahmed@me.com>
2021-08-03 23:44:44 +1000

    CP-4 removed the local package for strapi due to deployed version in another repo

--- packages/strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '377738c8cc6d596a95b2444aea85f86f'),
-    },
-  },
-});
