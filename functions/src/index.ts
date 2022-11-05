e93f4df4000e75a67e25b151d4f568f1cbd952f1
Fahad Ahmed <fahad.ahmed@me.com>
2022-11-05 10:34:27 +0530

    #1 updated schema to include stubs for posts in graphql

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,79 @@
 import * as functions from 'firebase-functions';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+const app = express();
+
+const posts = [
+  {
+    title: 'Dark/Light Theme using React, Emotion and Typescript',
+    slug: 'dark-light-theme-using-react-emotion-and-typescript',
+    tags: ['frontend', 'react', 'emotion'],
+    content: 'This is markdown content for the topic',
+    id: 1,
+  },
+  {
+    title: 'Setting up a modern React workflow with ViteJS',
+    slug: 'setting-up-a-modern-react-workflow-with-vitejs',
+    tags: ['build-tools', 'react', 'vite'],
+    content: 'This is markdown content for the topic',
+    id: 2,
+  },
+  {
+    title: 'Getting started with Remix.run',
+    slug: 'getting-started-with-remix-run',
+    tags: ['remix', 'react', 'fullstack'],
+    content: 'This is markdown content for the topic',
+    id: 3,
+  },
+  {
+    title: 'Integrating Firebase with Remix apps',
+    slug: 'integrating-firebase-with-remix-apps',
+    tags: ['remix', 'firebase', 'fullstack'],
+    content: 'This is markdown content for the topic',
+    id: 4,
+  },
+  {
+    title: 'Creating a magazine layout with CSS Grid',
+    slug: 'creating-a-magazine-layout-with-css-grid',
+    tags: ['css-grid', 'frontend'],
+    content: 'This is markdown content for the topic',
+    id: 5,
+  },
+];
+
+const schema = buildSchema(`
+  type Post {
+    title: String
+    slug: String
+    tags: [String]
+    content: String
+    id: Int
+  }
+  type Query {
+    hello: String
+    posts: [Post]
+  }
+`);
+
+var root = {
+  hello: () => {
+    return 'Hello World';
+  },
+  posts: () => {
+    return posts;
+  },
+};
+
+app.use(
+  '/',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+exports.graphql = functions.https.onRequest(app);
