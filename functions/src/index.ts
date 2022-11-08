1bed2616e033daf99f8e36fc6accb64ce57d8af1
Fahad Ahmed <fahad.ahmed@me.com>
2022-11-08 22:27:16 +0530

    #1 - setup apollo server on firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,14 @@
 import * as functions from 'firebase-functions';
+import type { ExpressContext } from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
+import type { Config } from 'apollo-server-cloud-functions';
+import { ApolloServer } from 'apollo-server-cloud-functions';
 
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+// const express = require('express');
+// const { graphqlHTTP } = require('express-graphql');
+// const { buildSchema } = require('graphql');
 
-const app = express();
+// const app = express();
 
 const posts = [
   {
@@ -44,7 +48,22 @@ const posts = [
   },
 ];
 
-const schema = buildSchema(`
+// const schema = buildSchema(`
+//   type Post {
+//     title: String
+//     slug: String
+//     tags: [String]
+//     content: String
+//     id: Int
+//   }
+//   type Query {
+//     hello: String
+//     getPosts: [Post]
+//     getPost(slug: String!): Post
+//   }
+// `);
+
+const typeDefs = gql`
   type Post {
     title: String
     slug: String
@@ -52,32 +71,47 @@ const schema = buildSchema(`
     content: String
     id: Int
   }
-  type Query {
-    hello: String
-    getPosts: [Post]
-    getPost(slug: String!): Post
-  }
-`);
 
-var root = {
-  hello: () => {
-    return 'Hello World';
-  },
-  getPosts: () => {
-    return posts;
-  },
-  getPost: (args: any) => {
-    return posts.find((post) => post.slug === args.slug);
+  type Query {
+    getPosts: [Post]
+    getPost(slug: String): Post
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getPosts: () => posts,
+    getPost: (args: any) => posts.find((post) => post.slug === args.slug),
   },
 };
 
-app.use(
-  '/',
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
+const graphqlConfig: Config<ExpressContext> = {
+  typeDefs,
+  resolvers,
+};
 
-exports.graphql = functions.https.onRequest(app);
+const server = new ApolloServer(graphqlConfig);
+const handler = server.createHandler();
+
+// var root = {
+//   hello: () => {
+//     return 'Hello World';
+//   },
+//   getPosts: () => {
+//     return posts;
+//   },
+//   getPost: (args: any) => {
+//     return posts.find((post) => post.slug === args.slug);
+//   },
+// };
+
+// app.use(
+//   '/',
+//   graphqlHTTP({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true,
+//   })
+// );
+
+exports.graphql = functions.https.onRequest(handler as any);
