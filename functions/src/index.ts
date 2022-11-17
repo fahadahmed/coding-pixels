3b7964d6180380dbc73fef57cfafb011211af9af
Fahad Ahmed <fahad.ahmed@me.com>
2022-11-17 17:27:09 +1100

    #1 - updated the graphql query to return post data from firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,43 +8,43 @@ import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
-const posts = [
-  {
-    title: 'Dark/Light Theme using React, Emotion and Typescript',
-    slug: 'dark-light-theme-using-react-emotion-and-typescript',
-    tags: ['frontend', 'react', 'emotion'],
-    content: 'This is markdown content for the topic',
-    id: 1,
-  },
-  {
-    title: 'Setting up a modern React workflow with ViteJS',
-    slug: 'setting-up-a-modern-react-workflow-with-vitejs',
-    tags: ['build-tools', 'react', 'vite'],
-    content: 'This is markdown content for the topic',
-    id: 2,
-  },
-  {
-    title: 'Getting started with Remix.run',
-    slug: 'getting-started-with-remix-run',
-    tags: ['remix', 'react', 'fullstack'],
-    content: 'This is markdown content for the topic',
-    id: 3,
-  },
-  {
-    title: 'Integrating Firebase with Remix apps',
-    slug: 'integrating-firebase-with-remix-apps',
-    tags: ['remix', 'firebase', 'fullstack'],
-    content: 'This is markdown content for the topic',
-    id: 4,
-  },
-  {
-    title: 'Creating a magazine layout with CSS Grid',
-    slug: 'creating-a-magazine-layout-with-css-grid',
-    tags: ['css-grid', 'frontend'],
-    content: 'This is markdown content for the topic',
-    id: 5,
-  },
-];
+// const posts = [
+//   {
+//     title: 'Dark/Light Theme using React, Emotion and Typescript',
+//     slug: 'dark-light-theme-using-react-emotion-and-typescript',
+//     tags: ['frontend', 'react', 'emotion'],
+//     content: 'This is markdown content for the topic',
+//     id: 1,
+//   },
+//   {
+//     title: 'Setting up a modern React workflow with ViteJS',
+//     slug: 'setting-up-a-modern-react-workflow-with-vitejs',
+//     tags: ['build-tools', 'react', 'vite'],
+//     content: 'This is markdown content for the topic',
+//     id: 2,
+//   },
+//   {
+//     title: 'Getting started with Remix.run',
+//     slug: 'getting-started-with-remix-run',
+//     tags: ['remix', 'react', 'fullstack'],
+//     content: 'This is markdown content for the topic',
+//     id: 3,
+//   },
+//   {
+//     title: 'Integrating Firebase with Remix apps',
+//     slug: 'integrating-firebase-with-remix-apps',
+//     tags: ['remix', 'firebase', 'fullstack'],
+//     content: 'This is markdown content for the topic',
+//     id: 4,
+//   },
+//   {
+//     title: 'Creating a magazine layout with CSS Grid',
+//     slug: 'creating-a-magazine-layout-with-css-grid',
+//     tags: ['css-grid', 'frontend'],
+//     content: 'This is markdown content for the topic',
+//     id: 5,
+//   },
+// ];
 
 const typeDefs = gql`
   type Post {
@@ -71,8 +71,15 @@ const resolvers = {
       });
       return posts;
     },
-    getPost: (parent: any, args: any, context: any, info: any) => {
-      return posts.find((post) => post.slug === args.slug);
+    getPost: async (parent: any, args: any, context: any, info: any) => {
+      const snapshot = await db.collection('posts').get();
+      let post;
+      snapshot.forEach((doc) => {
+        if (doc.data().slug === args.slug) {
+          post = doc.data();
+        }
+      });
+      return post;
     },
   },
 };
