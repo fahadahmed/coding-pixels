f5d82eeea3cb2786de52363823096e188c7c9eb7
Fahad Ahmed <fahad.ahmed@me.com>
2022-12-05 23:09:38 +1100

    fahadahmed/roadmap-personal#6 updates to the firebase config

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -4,25 +4,44 @@ import {
   initializeApp as initializeAdminApp,
 } from 'firebase-admin/app';
 import { initializeApp } from 'firebase/app';
-import { signInWithEmailAndPassword, getAuth, signOut } from 'firebase/auth';
+
+import {
+  signInWithEmailAndPassword,
+  getAuth,
+  signOut,
+  connectAuthEmulator,
+} from 'firebase/auth';
 
 require('dotenv').config();
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: process.env.CP_API_KEY,
+  authDomain: process.env.CP_AUTH_DOMAIN,
+  databaseURL: process.env.CP_DATABASE_URL,
+  projectId: process.env.CP_PROJECT_ID,
+  storageBucket: process.env.CP_STORAGE_BUCKET,
+  messagingSenderId: process.env.CP_MESSENGER_SENDER_ID,
+  appId: process.env.CP_APP_ID,
+  measurementId: process.env.CP_MEASUREMENT_ID,
+};
 
 if (!admin.apps.length) {
   initializeAdminApp({
     credential: applicationDefault(),
-    databaseURL: '',
+    databaseURL: process.env.CP_DATABASE_URL,
   });
 }
 
 const db = admin.firestore();
 const adminAuth = admin.auth();
 
-let Firebase;
+let Firebase: any;
 if (!Firebase?.apps?.length) {
   Firebase = initializeApp(firebaseConfig);
+}
+
+if (location.hostname === 'localhost') {
+  connectAuthEmulator(getAuth(), 'http://localhost:9099');
 }
 
 async function signIn(email: string, password: string) {
